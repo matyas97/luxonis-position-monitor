7172a8e956b17382f10507c947501509f30b0d86
matyas97 <matyaskrecek@icloud.com>
2022-11-12 11:55:33 +0100

    Created ThreeJS object positioned by fetching gyroscope data

--- src/Camera.tsx
+++ src/Camera.tsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import * as THREE from 'three';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
 
 interface GyroscopeData {
   x: number;
@@ -6,42 +9,73 @@ interface GyroscopeData {
   z: number;
 }
 
-const mockGyroscopeData: GyroscopeData = {
-  x: 0.00532632227987051,
-  y: 0.022370552644133568,
-  z: 0.0,
-};
-
 const Camera = () => {
   const APP_URL = window.location.origin + window.location.pathname;
   const GYROSCOPE_DATA_URL = APP_URL + 'gyroscope';
 
   const [gyroscopeData, setGyroscopeData] =
     React.useState<GyroscopeData | null>(null);
-  console.log('gyroscopeData', gyroscopeData);
 
   React.useEffect(() => {
-    const request = fetch(GYROSCOPE_DATA_URL, {
-      method: 'GET',
-    });
-
-    request.then((response) => {
-      const json = response.json();
-      json.then((data: GyroscopeData) => {
-        setGyroscopeData(data);
+    const interval = setInterval(() => {
+      const request = fetch(GYROSCOPE_DATA_URL, {
+        method: 'GET',
       });
-    });
+
+      request.then((response) => {
+        const json = response.json();
+        json.then((data: GyroscopeData) => {
+          setGyroscopeData(data);
+        });
+      });
+    }, 100);
+
+    return () => clearInterval(interval);
   }, []);
 
+  return gyroscopeData ? (
+    <Canvas>
+      <ambientLight />
+      <spotLight intensity={0.8} position={[5, 10, 50]} />
+      <CameraMesh x={gyroscopeData.x} y={gyroscopeData.y} z={gyroscopeData.z} />
+      <OrbitControls />
+    </Canvas>
+  ) : (
+    <div style={styles.infoDiv}>Can't access gyroscrope data...</div>
+  );
+};
+
+const styles = {
+  infoDiv: {
+    display: 'flex',
+    justifyContent: 'center',
+    color: 'white',
+  },
+};
+
+const CameraMesh: React.FC<GyroscopeData> = ({ x, y, z }) => {
+  const ref = React.useRef<THREE.Mesh>();
+
+  /* const mockGyroscopeData: GyroscopeData = {
+    x: 0.022370552644133568,
+    y: 0.007456851191818714,
+    z: 0.023435818031430244,
+  }; */
+
+  useFrame(() => {
+    if (ref.current) {
+      ref.current.rotation.x = x * 100;
+      ref.current.rotation.y = y * 100;
+      ref.current.rotation.z = z * 100;
+    }
+  });
+
   return (
-    <div>
-      <h2>Camera view</h2>
-      <div>
-        <p>x: {gyroscopeData?.x}</p>
-        <p>y: {gyroscopeData?.y}</p>
-        <p>z: {gyroscopeData?.z}</p>
-      </div>
-    </div>
+    // @ts-ignore
+    <mesh ref={ref}>
+      <boxGeometry args={[4, 1, 1]} />
+      <meshPhysicalMaterial color="lightgrey" />
+    </mesh>
   );
 };
 
